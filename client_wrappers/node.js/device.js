1617870720ccfaa6d07644b2a659051fcb57133e
brad <bradmedeiros0@gmail.com>
2015-10-08 00:54:54 -0400

    device client state rep

--- client_wrappers/node.js/device.js
+++ client_wrappers/node.js/device.js
@@ -10,42 +10,57 @@ var device = function () {
 **/
 function convert_to_string_array ( element ){
 	//@todo
-};
+	var array ;
+	if (!Array.isArray(element)){
+		array = [ ];
+		array.push(element);
+	}else{
+		array = element;
+	}
+	return array;
+}
 
 device.prototype.add_subscriptions =  function ( subscriptions ){
-	var subscriptions_to_add = convert_to_array( subscriptions );
-	for ( element in subscriptions_to_add ){
+	var subscriptions_to_add = convert_to_string_array( subscriptions );
+	for ( var element in subscriptions_to_add ){
 		this.subscriptions.push(subscriptions_to_add[element]);
 	}
 };
 
 device.prototype.remove_subscriptions = function ( subscriptions ) {
-	var subscriptions_to_remove = convert_to_array(subscriptions);
-	// @todo
-
+	var subscriptions_to_remove = convert_to_string_array(subscriptions);
+	for ( var element in subscriptions_to_remove ){
+		var index = this.subscriptions.indexOf(subscriptions_to_remove[element]);
+		if (index > -1 ){
+			this.subscriptions.splice(index,1);
+		}
+	}
 };
 
 device.prototype.get_subscriptions = function ( ){
 	return this.subscriptions;
-}
+};
 
 device.prototype.add_publications = function  ( publications ){
-	var publications_to_add = convert_to_array(subscriptions);
-	for ( element in publications_to_add ){
+	var publications_to_add = convert_to_string_array(publications);
+	for ( var element in publications_to_add ){
 		this.publications.push(publications_to_add[element]);
 	}
 };
 
 device.prototype.remove_publications = function ( publications ){
-	var publications_to_remove = convert_to_array( publications);
-	//@todo
-
+	var publications_to_remove = convert_to_string_array(publications);
+	for ( var element in publications_to_remove ){
+		var index = this.publications.indexOf(publications_to_remove[element]);
+		if (index > -1 ){
+			this.publications.splice(index ,1);
+		}
+	}
 };
 
 device.prototype.get_publications = function ( ){
 	return this.publications;
-}
+};
 
-fail();
 
 module.exports = device;
