91bab3e8be21f5939988529bf6cf5b475d4f0a1c
brad <bradmedeiros0@gmail.com>
2016-02-20 18:02:03 -0500

    fixed wildcards

--- client_wrappers/javascript/.common/automate_common.js
+++ client_wrappers/javascript/.common/automate_common.js
@@ -261,7 +261,26 @@ function get_matched_subscriptions (subscriptions, field){
 }
 
 function is_wildcard_match(str, rule) {
-  return new RegExp("^" + rule.replace("*", ".*") + "$").test(str);
+  return globStringToRegex(rule).test(str);
+}
+
+function globStringToRegex(str) {
+    return new RegExp(preg_quote(str).replace(/\\\*/g, '.*').replace(/\\\?/g, '.'), 'g');
+}
+function preg_quote (str, delimiter) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: booeyOH
+    // +   improved by: Ates Goral (http://magnetiq.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // *     example 1: preg_quote("$40");
+    // *     returns 1: '\$40'
+    // *     example 2: preg_quote("*RRRING* Hello?");
+    // *     returns 2: '\*RRRING\* Hello\?'
+    // *     example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+    // *     returns 3: '\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:'
+    return (str + '').replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
 }
 
 
